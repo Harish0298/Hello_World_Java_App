6306188c90953e03d042124df2e7a309278923af
harish0298 <harishshankar221@gmail.com>
2025-03-26 23:31:58 +0100

    Adding pom.xml and HelloWorld.java file

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+
+
+// src/main/java/com/example/HelloWorld.java
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+    }
+}
